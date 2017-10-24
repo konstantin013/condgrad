784dcdae8eb96e92bddd8ce39025310f7bb24959
konstantin <konstantin_013@mail.ru>
2017-10-24 22:51:17 +0300

    create class Armijo and insert it as one_dim_method but now this class return primary 0.001

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import numpy as np
 from scipy import optimize
 
+class Armijo:
+    def __init__(self, e, alpha_s, theta):
+        self.e = e
+        self.alpha_s = alpha_s
+        self.theta = theta
+    def __call__(self, f, fd, x, d):
+        return 0.001
 
-def condgrad(u0, f, df, A_ub=None, b_ub=None, A_eq=None, b_eq=None):
+
+
+
+def condgrad(u0, f, df, one_dim_method, A_ub=None, b_ub=None, A_eq=None, b_eq=None):
     print(u0)
     bnd = [(None, None)] * u0.size
-    alpha = 0.001
 
     for i in range(10000):
         #print('step: ' + str(i))
@@ -14,6 +23,7 @@ def condgrad(u0, f, df, A_ub=None, b_ub=None, A_eq=None, b_eq=None):
 
         u = optimize.linprog(c, A_ub, b_ub, A_eq, b_eq, bounds=bnd).x
         #print(u)
+        alpha = one_dim_method(f, df, u0, u - u0)
         u1 = u0 + alpha * (u - u0)
         print(u1)
         u0 = u1
@@ -42,4 +52,6 @@ def df(u):
     return np.array([2 * (x + 0.3), 2 * (y - 0.7)])
 u0 = np.array([0, 0], dtype='float64')
 
-condgrad(f=f, df=df, A_ub=A_ub, b_ub=b_ub, u0=u0)
+armijo = Armijo(0.3, 0.6, 0.9)
+
+condgrad(f=f, df=df, one_dim_method=armijo, A_ub=A_ub, b_ub=b_ub, u0=u0)
